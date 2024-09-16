340bb3f7fbf8c4e1c2a0c6adb8e7c8aa97d20944
Inkvisto <qwertuasdfgh953@gmail.com>
2024-09-16 14:35:22 +0300

    basic implementatiopn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// --- (1), (2) & (3): install and import ---
+import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+// --- ---------------------------------- ---
 
-function App() {
+export function App() {
+  const position = [48.51, 31.38];
+
+  const fillBlueOptions = { fillColor: "blue" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Circle center={position} pathOptions={fillBlueOptions} radius={400} />
+      </MapContainer>
     </div>
   );
 }
